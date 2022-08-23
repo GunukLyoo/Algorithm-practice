0e7f74c3d572e4f357ed70152e7e4ba0562516b7
GunukLyoo <gunuk418@gmail.com>
2022-08-23 22:25:20 +0900

    practice

--- Algorithm/src/edu/Q1712.java
+++ Algorithm/src/edu/Q1712.java
@@ -5,19 +5,23 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Q3053 {
+public class Q1712 {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int r;
-		double U = 0, T = 0;
+		int A, B, C;
 		
-		r = Integer.parseInt(br.readLine());
-		U = Math.PI * r * r;
-		T = 2 * r * r;
+		String[] str = br.readLine().split(" ");
+		A = Integer.parseUnsignedInt(str[0]);
+		B = Integer.parseUnsignedInt(str[1]);
+		C = Integer.parseUnsignedInt(str[2]);
+
+		if(B>=C) bw.write("-1");
+		else {
+			bw.write(Integer.toUnsignedString((A/(C-B))+1));
+		}
 		
-		bw.write(String.format("%.6f", U) + "\n" + String.format("%.6f", T));
 		bw.close();
 	}
 
