8c7a121387c6c4c94c2754d53568f8d04a50e3f0
GunukLyoo <gunuk418@gmail.com>
2022-09-23 23:30:48 +0900

    need to think more

--- Algorithm/src/edu/Q11660.java
+++ Algorithm/src/edu/Q11660.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class Q11660 {
 
@@ -13,15 +14,28 @@ public class Q11660 {
 		String[] arr = br.readLine().split(" ");
 		int n = Integer.parseInt(arr[0]);
 		int[][] nn = new int[n][n];
+		int[][] totalyn = new int[n][n];
 		int m = Integer.parseInt(arr[1]);
 		int x1, x2, y1, y2;
 		int sum = 0;
+		int c = 0;
+		
+		for(int i = 0; i < n; i++) {
+			Arrays.fill(totalyn[i], 0);
+		}
 		
 		for(int i = 0; i < n; i++) {
 			arr = br.readLine().split(" ");
 			for(int j = 0; j < n; j++) {
 				nn[i][j] = Integer.parseInt(arr[j]);
+				if(c == 0) {
+					totalyn[c][j] = nn[i][j];
+				}else {
+					totalyn[c][j] = totalyn[c-1][j] + nn[i][j];
+				}
+			
 			}
+			c = c + 1;
 		}
 		
 		for(int i = 0; i < m; i++) {
@@ -30,11 +44,24 @@ public class Q11660 {
 			y1 = Integer.parseInt(arr[1]);
 			x2 = Integer.parseInt(arr[2]);
 			y2 = Integer.parseInt(arr[3]);
-			for(int j = y1 - 1; j < y2; j++) {
+			/*for(int j = y1 - 1; j < y2; j++) {
 				for(int k = x1 - 1; k < x2; k++) {
 					sum = sum + nn[k][j];
 				}
+			}*/
+			if(x1 == x2 && y1 == y2) {
+				sum = nn[x1-1][y1-1];
 			}
+			else {
+			for(int j = x1-1 ; j < x2; j++) {
+				if(y1 == 1) {
+					sum = sum + totalyn[j][n-1];
+				}else {
+					sum = sum + totalyn[j][n-1];// - totalyn[j][y2 - 2];
+				}
+			}
+			}
+			
 			bw.write(Integer.toString(sum) + "\n");
 			bw.flush();
 			sum = 0;
