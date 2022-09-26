88956acf2fd4e0732b0f0f9b9c36dff2821dcf09
GunukLyoo <gunuk418@gmail.com>
2022-09-26 22:50:44 +0900

    practice

--- Algorithm/src/edu/Q1181.java
+++ Algorithm/src/edu/Q1181.java
@@ -5,6 +5,9 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Q1181 {
 
@@ -16,20 +19,29 @@ public class Q1181 {
 		
 		n = Integer.parseInt(br.readLine());
 		String[] arr = new String[n];
-		int[][] count = new int[50][n];
 		
 		while(c < n) {
 			arr[c] = br.readLine();
 			c = c + 1;
 		}
 		
-	//	Arrays.sort(arr, (s1, s2) -> {
+		Set<String> arrs = new HashSet<String>(Arrays.asList(arr));
+		arr = arrs.toArray(new String[0]);
+		
+		Arrays.sort(arr, new Comparator<String>() {
 			
-		//});
+			public int compare(String s1, String s2) {
+				if(s1.length() != s2.length()) {
+					return s1.length() - s2.length();
+				}else {
+					return s1.compareTo(s2);
+				}
+			}
+		});
 		c = 0;
 		
-		while(c < n) {
-	//		bw.write(arr[c] + "\n");
+		while(c < arr.length) {
+			bw.write(arr[c] + "\n");
 			c = c + 1;
 		}
 		bw.close();
