30ed25796c57d682256b9d73ec30022580b387c5
GunukLyoo <gunuk418@gmail.com>
2022-09-29 23:18:25 +0900

    refactoring
    need to make more short

--- Algorithm/src/edu/Q18870.java
+++ Algorithm/src/edu/Q18870.java
@@ -4,9 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
 
 public class Q18870 {
 
@@ -15,19 +13,18 @@ public class Q18870 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int n = Integer.parseInt(br.readLine());
 		Integer[] arr = new Integer[n];
+		ArrayList<Integer> al = new ArrayList<>();
 		String[] arrs = br.readLine().split(" ");
 		
 		for(int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(arrs[i]);
+			if(al.contains(arr[i]) == false) al.add(arr[i]);
 		}
 		
-		Set<Integer> set = new HashSet<Integer>(Arrays.asList(arr));
-		Integer[] arrc = set.toArray(new Integer[0]);
-		
-		Arrays.sort(arrc);
-		
+		al.sort(null);
+
 		for(int i = 0; i < n; i++) {
-			bw.write(Integer.toString(Arrays.asList(arrc).indexOf(arr[i])) + " ");
+			bw.write(Integer.toString(al.indexOf(arr[i])) + " ");
 		}
 		bw.close();
 	}
