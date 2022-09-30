97d55fd9d26f16d3c9c4fe7f3f3d5a712fa1c585
GunukLyoo <gunuk418@gmail.com>
2022-09-30 23:26:09 +0900

    practice

--- Algorithm/src/edu/Q18870.java
+++ Algorithm/src/edu/Q18870.java
@@ -4,28 +4,42 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class Q18870 {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		int n = Integer.parseInt(br.readLine());
+		Integer[] sorted = new Integer[n];
 		Integer[] arr = new Integer[n];
-		ArrayList<Integer> al = new ArrayList<>();
-		String[] arrs = br.readLine().split(" ");
+		HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
 		
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		for(int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(arrs[i]);
-			if(al.contains(arr[i]) == false) al.add(arr[i]);
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		al.sort(null);
-
-		for(int i = 0; i < n; i++) {
-			bw.write(Integer.toString(al.indexOf(arr[i])) + " ");
+		sorted = arr.clone();
+		int id = 0;
+		Arrays.sort(sorted);
+		
+		for(int i : sorted) {
+			if(!hm.containsKey(i)) {
+				hm.put(i, id);
+				id = id + 1;
+			}
 		}
+		
+		for(int i : arr) {
+			sb.append(hm.get(i)).append(" ");
+		}
+		bw.write(sb.toString());
 		bw.close();
 	}
 
