c1984ca676b51becbd0984ed93290c7f264aec59
GunukLyoo <gunuk418@gmail.com>
2022-10-02 22:56:01 +0900

    practice

--- Algorithm/src/edu/Q10828.java
+++ Algorithm/src/edu/Q10828.java
@@ -4,12 +4,17 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 
 public class Q10828 {
+	ArrayList<Integer> stack = new ArrayList<>();
+	
+	public void
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
 
 	}
 
