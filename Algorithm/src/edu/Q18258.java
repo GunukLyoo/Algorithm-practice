86f2b185d1eb56dc6240683bcf598c8db10d63d5
GunukLyoo <gunuk418@gmail.com>
2022-10-18 23:28:26 +0900

    need to make faster

---
+++ Algorithm/src/edu/Q18258.java
@@ -0,0 +1,69 @@
+package edu;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+public class Q18258 {
+	static ArrayList<Integer> stack = new ArrayList<>();
+	
+	public static void push(int num) {
+		stack.add(num);
+	}
+	
+	public static void pop() {
+		if(stack.isEmpty()) System.out.println("-1");
+		else {
+			//int i = stack.size() - 1;
+			System.out.println(stack.get(0));
+			stack.remove(0);
+		}
+	}
+	
+	public static void size() {
+		System.out.println(stack.size());
+	}
+	
+	public static void empty() {
+		if(stack.isEmpty()) System.out.println("1");
+		else System.out.println("0");
+	}
+	
+	public static void front() {
+		if(stack.isEmpty()) System.out.println("-1");
+		else System.out.println(stack.get(0));
+	}
+	
+	public static void back() {
+		if(stack.isEmpty()) System.out.println("-1");
+		else System.out.println(stack.get(stack.size()-1));
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		String[] arr;
+		
+		for(int i = 0; i < n; i++) {
+			arr = br.readLine().split(" ");
+			switch (arr[0]) {
+				case "push": push(Integer.parseInt(arr[1]));
+							break;
+				case "pop": pop();
+							break;
+				case "size": size();
+							break;
+				case "empty": empty();
+							break;
+				case "front": front();
+							break;
+				case "back": back();
+							break;
+			}
+		}
+
+	}
+}
