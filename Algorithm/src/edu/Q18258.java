3e5bfd52ea334dab11d377567b67cb85b219d349
GunukLyoo <gunuk418@gmail.com>
2022-10-23 22:55:15 +0900

    practice

--- Algorithm/src/edu/Q18258.java
+++ Algorithm/src/edu/Q18258.java
@@ -9,35 +9,38 @@ import java.util.ArrayList;
 public class Q18258 {
 	static ArrayList<Integer> stack = new ArrayList<>();
 	static StringBuilder sb = new StringBuilder();
+	static int count = 0;
 	
 	public static void push(int num) {
 		stack.add(num);
+		count++;
 	}
 	
 	public static void pop(BufferedWriter bw){
-		if(stack.isEmpty()) sb.append("-1\n");
+		if(count == 0) sb.append("-1\n");
 		else {
 			sb.append(Integer.toString(stack.get(0)) + "\n");
 			stack.remove(0);
+			count--;
 		}
 	}
 	
 	public static void size(BufferedWriter bw){
-		sb.append(Integer.toString(stack.size())+ "\n");
+		sb.append(Integer.toString(count)+ "\n");
 	}
 	
 	public static void empty(BufferedWriter bw){
-		if(stack.isEmpty()) sb.append("1\n");
+		if(count == 0) sb.append("1\n");
 		else sb.append("0\n");
 	}
 	
 	public static void front(BufferedWriter bw){
-		if(stack.isEmpty()) sb.append("-1\n");
+		if(count == 0) sb.append("-1\n");
 		else sb.append(Integer.toString(stack.get(0))+ "\n");
 	}
 	
 	public static void back(BufferedWriter bw){
-		if(stack.isEmpty()) sb.append("-1\n");
+		if(count == 0) sb.append("-1\n");
 		else sb.append(Integer.toString(stack.get(stack.size()-1))+ "\n");
 	}
 
