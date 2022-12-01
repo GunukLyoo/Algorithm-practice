d354669fa2c3e061369ea5b64db2bf06329691e9
GunukLyoo <gunuk418@gmail.com>
2022-12-01 23:50:44 +0900

    to python

---
+++ to python/Q11021.py
@@ -0,0 +1,8 @@
+import sys
+
+if __name__ == "__main__":
+    n = int(sys.stdin.readline())
+
+    for i in range(0,n):
+        a, b = map(int, sys.stdin.readline().split())
+        print("Case #" + str(i+1) + " " + str(a + b) )
