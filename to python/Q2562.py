e433348744171c9fe293ec4e63b9a3bc0172c19c
GunukLyoo <gunuk418@gmail.com>
2022-12-13 23:11:39 +0900

    to python

--- to python/Q2562.py
+++ to python/Q2562.py
@@ -2,19 +2,21 @@ import sys
 
 if __name__ == "__main__":
 
-    n = int(sys.stdin.readline())
-    num_list = list(map(int, sys.stdin.readline().split()))
-    max = num_list[0];
-    min = num_list[0];
-    
+    num_list = []
 
-    for i in range(1, n):
-        if num_list[i] < min:
-            min = num_list[i]
-        elif num_list[i] > max:
+    for i in range(0, 9):
+        num_list.append(int(sys.stdin.readline()))
+
+    max = num_list[0]
+    maxi = 1
+
+    for i in range(1, 9):
+        if num_list[i] > max:
             max = num_list[i]
+            maxi = i + 1
 
-    print(str(min) + ' ' + str(max))
+    print(max)
+    print(maxi)
     
     
         
