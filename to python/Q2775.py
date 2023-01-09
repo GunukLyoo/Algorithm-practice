d29f72733433a62bcb91b25d4019634cda7c6bce
GunukLyoo <gunuk418@gmail.com>
2023-01-09 22:59:07 +0900

    to python ~ing

--- to python/Q2775.py
+++ to python/Q2775.py
@@ -21,9 +21,9 @@ if __name__ == "__main__":
 
     for i in range(1,n):
         if maxk < num[i][0]:
-            maxk = num[i][0];
-	elif maxn < num[i][1]):
-            maxn = num[i][1];
+            maxk = num[i][0]
+        elif maxn < num[i][1]:
+            maxn = num[i][1]
 
     table = list()
 
@@ -33,3 +33,13 @@ if __name__ == "__main__":
         a.append(sum)
 
     table.append(a)
+
+    for i in range(maxk):
+        a = list()
+        for j in range(maxn):
+            sum += table[i][j]
+            a.append(sum)
+        table.append(a)
+
+    for i in range(n):
+        print(table[num[i][0]][num[i][1] - 1])
