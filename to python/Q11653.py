b66cafdcb3e076b13f4a4d561f552bfc2191925c
GunukLyoo <gunuk418@gmail.com>
2023-01-18 23:20:27 +0900

    need to find another way

--- to python/Q11653.py
+++ to python/Q11653.py
@@ -1,27 +1,15 @@
 import sys
+from primePy import primes
 
 if __name__ == "__main__":
 
     n = int(sys.stdin.readline())
-    c = 2
-    max = n
-
-    prime = list()
-    prime.append(False)
-    prime.append(False)
-
-    for i in range(2, n+1):
-        prime.append(True)
+    c = 0
+    prime = primes.upto(n)
 
     while n != 1:
-        if n%c == 0:
-            n /= c
-            print(c)
+        if n%prime[c] == 0:
+            n /= prime[c]
+            print(prime[c])
         else:
             c += 1
-            if prime[c] == True:
-                for j in range(c*c, max+1, c):
-                    prime[j] = False
-            else:
-                while prime[c] != False:
-                    c += 1
