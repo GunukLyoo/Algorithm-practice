1edbed7129c44807f99e33ce90009b6637afcc03
GunukLyoo <gunuk418@gmail.com>
2023-01-20 23:23:01 +0900

    to python

--- to python/Q11653.py
+++ to python/Q11653.py
@@ -1,17 +1,9 @@
 import sys
-import math
-
-def prime(x):
-    for i in range(2, int(math.sqrt(x)+1)):
-        if x % i == 0:
-            return False
-    return True
 
 if __name__ == "__main__":
 
     n = int(sys.stdin.readline())
     c = 2
-    max = n
 
     while n != 1:
         if n%c == 0:
@@ -19,6 +11,3 @@ if __name__ == "__main__":
             print(c)
         else:
             c += 1
-            if prime(c) == False:
-                while prime(c) == False:
-                    c += 1
