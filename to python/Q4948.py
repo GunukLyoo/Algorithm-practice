e3384e8431c7aa3585a85be3f0e85fe6434a3ad6
GunukLyoo <gunuk418@gmail.com>
2023-01-23 22:33:15 +0900

    to python

--- to python/Q4948.py
+++ to python/Q4948.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         num.append(c)
         c = int(sys.stdin.readline())
 
-    max = int(123456)
+    max = int(246913)
 
     prime = list()
     prime.append(False)
@@ -24,13 +24,12 @@ if __name__ == "__main__":
                 prime[j] = False
 
     for i in num:
-        print(i)
         c = 0
         if 2*i <= max:
             for j in range(i+1, 2*i + 1):
                 if prime[j]:
                     c += 1
-        elif 2*i > max:
+        else:
             for j in range(i+1, max+1):
                 if prime[j]:
                     c += 1
