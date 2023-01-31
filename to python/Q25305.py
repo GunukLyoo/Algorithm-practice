8308e6865241e585870b2bbb8bedada949793fb6
GunukLyoo <gunuk418@gmail.com>
2023-01-31 22:37:47 +0900

    to python

--- to python/Q25305.py
+++ to python/Q25305.py
@@ -3,4 +3,18 @@ import sys
 if __name__ == "__main__":
 
     n = list(map(int, sys.stdin.readline().split()))
-    score = n = list(map(int, sys.stdin.readline().split()))
+    score = list(map(int, sys.stdin.readline().split()))
+
+    for i in range(n[0]):
+        max = score[i]
+        maxi = i
+        for j in range(i+1, n[0]):
+            if max < score[j]:
+                max = score[j]
+                maxi = j
+
+        x = score[i]
+        score[i] = max
+        score[maxi] = x
+
+    print(score[n[1]-1])
