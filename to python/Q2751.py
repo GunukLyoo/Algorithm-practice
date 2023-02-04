1b5971efd376ee598e286d11f62ea72beebc16d0
GunukLyoo <gunuk418@gmail.com>
2023-02-04 23:26:47 +0900

    recursion error

--- to python/Q2751.py
+++ to python/Q2751.py
@@ -1,28 +1,29 @@
 import sys
 
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    lesser_arr, equal_arr, greater_arr = [], [], []
+    for num in arr:
+        if num < pivot:
+            lesser_arr.append(num)
+        elif num > pivot:
+            greater_arr.append(num)
+        else:
+            equal_arr.append(num)
+    return quick_sort(lesser_arr) + equal_arr + quick_sort(greater_arr)
+
 if __name__ == "__main__":
 
     m = int(sys.stdin.readline())
     arr1 = list()
-    sum = 0
 
     for i in range(m):
         n = int(sys.stdin.readline())
         arr1.append(n)
-        sum += n
+
+    arr = quick_sort(arr1)
 
     for i in range(m):
-        min = arr1[i]
-        mini = i
-        for j in range(i+1, m):
-            if min > arr1[j]:
-                min = arr1[j]
-                mini = j
-
-        x = arr1[i]
-        arr1[i] = min
-        arr1[mini] = x
-
-    
-    for i in range(m):
-        print(arr1[i])
+        print(arr[i])
