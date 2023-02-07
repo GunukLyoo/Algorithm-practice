84e5c6ffff8be5fa7a620a8c0a061d892cb10f09
GunukLyoo <gunuk418@gmail.com>
2023-02-07 23:04:55 +0900

    to python

---
+++ to python/Q11650.py
@@ -0,0 +1,18 @@
+import sys
+
+if __name__ == "__main__":
+
+    m = int(sys.stdin.readline())
+    arr = list()
+
+    for i in range(m):
+        arr.append(list(map(int, sys.stdin.readline().split())))
+
+    arr1 = sorted(arr, key = lambda x : (x[0], x[1]))
+
+    for i in range(m):
+        print(arr1[i][0], end = ' ')
+        print(arr1[i][1])
+        
+
+    
