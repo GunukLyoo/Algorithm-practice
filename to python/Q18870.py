cf4bbd733ad04a55ed5ed831286c84661c73eef8
GunukLyoo <gunuk418@gmail.com>
2023-02-12 23:38:57 +0900

    trying

--- to python/Q18870.py
+++ to python/Q18870.py
@@ -5,13 +5,18 @@ if __name__ == "__main__":
     m = int(sys.stdin.readline())
     arr = list(map(int, sys.stdin.readline().split()))
     id = 0
+    c = 0
+    ind = 0
 
     arr1 = sorted(list(set(arr)))
 
     for i in range(len(arr1)):
-        for j in range(m):
-            if arr1[i] == arr[j]:
-                arr[j] = id
+        if arr.count(arr1[i]) > 1:
+            while c < arr.count(arr1[i]):
+                arr[arr.index(arr1[i], ind)] = id
+                ind = arr.index(arr1[i], ind) + 1
+        else:
+            arr[arr.index(arr1[i])] = id
         id += 1
 
     for i in range(m):
