e6faa0f0688dcebbbebe369f130cc1a95322b04d
GunukLyoo <gunuk418@gmail.com>
2023-02-15 23:01:29 +0900

    need to make faster

--- to python/Q18870.py
+++ to python/Q18870.py
@@ -5,21 +5,22 @@ if __name__ == "__main__":
     m = int(sys.stdin.readline())
     arr = list(map(int, sys.stdin.readline().split()))
     id = 0
-    c = 0
-    ind = 0
 
     arr1 = sorted(list(set(arr)))
 
     for i in range(len(arr1)):
-        if arr.count(arr1[i]) > 1:
-            while ind < len(arr):#arr.count(arr1[i]):
-                arr[arr.index(arr1[i], ind)] = id
-                print(id)
-                ind = arr.index(arr1[i], ind)
+        c = arr.count(arr1[i])
+        ind = 0
+        n = 0
+        if c > 1:
+            while c != 0:
+                n = arr.index(arr1[i], ind)
+                arr[n] = id
+                ind = n + 1
+                c -= 1
         else:
             arr[arr.index(arr1[i])] = id
         id += 1
-        ind = 0
 
     for i in range(m):
         print(arr[i], end = ' ')
