8c6c843574e915054f6c08a65e1ee0c7db960dbe
GunukLyoo <gunuk418@gmail.com>
2023-03-03 23:56:33 +0900

    quick sort test

--- to python/Q10815.py
+++ to python/Q10815.py
@@ -17,6 +17,20 @@ def binary_search(target, data):
 
     return None
 
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    lesser_arr, equal_arr, greater_arr = [], [], []
+    for num in arr:
+        if num < pivot:
+            lesser_arr.append(num)
+        elif num > pivot:
+            greater_arr.append(num)
+        else:
+            equal_arr.append(num)
+    return quick_sort(lesser_arr) + equal_arr + quick_sort(greater_arr)
+
 if __name__ == "__main__":
 
     n = int(sys.stdin.readline())
@@ -24,10 +38,10 @@ if __name__ == "__main__":
     m = int(sys.stdin.readline())
     arr1 = list(map(int, sys.stdin.readline().split()))
 
-    arr.sort()
+    qarr = quick_sort(arr)
 
     for i in range(m):
-        res = binary_search(arr1[i], arr)
+        res = binary_search(arr1[i], qarr)
         if res == None:
             print("0", end=' ')
         else:
