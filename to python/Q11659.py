d916969a53beccb11e9d28c1c3524711f174c5e7
GunukLyoo <gunuk418@gmail.com>
2023-03-08 23:18:47 +0900

    to python

--- to python/Q11659.py
+++ to python/Q11659.py
@@ -1,32 +1,22 @@
 import sys
 
-def xtoy(arr, x, y, total):
-    sum = 0
-		
-    if y-x+1 < arr.length/2:
-        for i in range(x-1, y):
-            sum = sum + arr[i]
-	return sum
-    else:
-        if x != 1:
-            for i in range(x-1):
-                sum += arr[i]
-			
-        for i in range(y, len(arr)):
-            sum += arr[i]
-
-	return total - sum;
-
 if __name__ == "__main__":
     arr = list(map(int, sys.stdin.readline().split()))
     arr1 = list(map(int, sys.stdin.readline().split()))
+    total = 0
+    sum = list()
+
+    for i in range(len(arr1)):
+        if i == 0:
+            sum.append(arr1[i])
+        else:
+            sum.append(sum[i-1] + arr1[i])
 
     for i in range(arr[1]):
         arr2 = list(map(int, sys.stdin.readline().split()))
-        if arr2[0] == arr2[1]:
-            print(arr1[arr2[0]-1])
+
+        if arr2[0] == 1:
+            print(sum[arr2[1]-1])
         else:
-            sum = 0
-            for j in range(arr2[0]-1, arr2[1]):
-                sum += arr1[j]
-            print(sum)
+            print(sum[arr2[1]-1] - sum[arr2[0]-2])
+        
