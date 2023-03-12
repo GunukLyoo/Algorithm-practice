9c454ab54d6dac34209056af584c131130662cf7
GunukLyoo <gunuk418@gmail.com>
2023-03-12 22:41:57 +0900

    need to make faster

--- to python/Q10986.py
+++ to python/Q10986.py
@@ -1,11 +1,15 @@
 import sys
-from bisect import bisect_left, bisect_right
     
 if __name__ == "__main__":
     n, m = map(int, sys.stdin.readline().split())
     arr = list(map(int, sys.stdin.readline().split()))
     c = 0
-    arr2 = list()
 
     for i in range(n):
-        for j in range(i)
+        sum = 0
+        for j in range(i, n):
+            sum += arr[j]
+            if sum%m == 0:
+                c += 1
+
+    print(c)
