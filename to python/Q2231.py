c45c3f137343796cdcca5eba0492474aacaab7e1
GunukLyoo <gunuk418@gmail.com>
2023-03-18 23:03:46 +0900

    need to make faster
    plus print(0)

--- to python/Q2231.py
+++ to python/Q2231.py
@@ -1,9 +1,9 @@
 import sys
-import math
 
 if __name__ == '__main__':
     n = int(sys.stdin.readline())
     m = n - (len(str(n))*9)
+    check = False
 
     while True:
         arr = list(map(int, str(m)))
@@ -12,7 +12,12 @@ if __name__ == '__main__':
             total += arr[i]
 
         if total == n:
-            print(m)
+            check = True
             break
         else:
             m += 1
+
+    if check == True:
+        print(m)
+    else:
+        print(0)
