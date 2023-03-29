5188edd7cd69bbd894d8aef28663fc1b8c135768
GunukLyoo <gunuk418@gmail.com>
2023-03-29 23:30:34 +0900

    to python

--- to python/Q9063.py
+++ to python/Q9063.py
@@ -7,9 +7,27 @@ if __name__ == '__main__':
     for i in range(n):
         xy.append(list(map(int, sys.stdin.readline().split())))
 
-    maxx = xy[0][0]
-    maxy = xy[0][1]
-    minx = xy[0][0]
-    miny = xy[0][1]
-    
+    if n == 1:
+        print(0)
+    else:
+
+        maxx = xy[0][0]
+        maxy = xy[0][1]
+        minx = xy[0][0]
+        miny = xy[0][1]
+
+        for i in range(n):
+            if maxx < xy[i][0]:
+                maxx = xy[i][0]
+
+            if maxy < xy[i][1]:
+                maxy = xy[i][1]
+
+            if minx > xy[i][0]:
+                minx = xy[i][0]
+
+            if miny > xy[i][1]:
+                miny = xy[i][1]
+        
+        print((maxx-minx) * (maxy - miny))
     
