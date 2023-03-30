837393cbbc18cfefcddf68d7b59036e33b3b0ca4
GunukLyoo <gunuk418@gmail.com>
2023-03-30 23:31:53 +0900

    something wrong

---
+++ to python/Q10101.py
@@ -0,0 +1,24 @@
+import sys
+
+if __name__ == '__main__':
+    angle = list()
+    total = 0
+
+    for i in range(3):
+        angle.append(int(sys.stdin.readline()))
+        total += angle[i]
+
+    if total != 180:
+        print("Error")
+    else:
+        angle.sort()
+
+        if angle[0] == angle[1] or angle[1] == angle[2]:
+            print("Isosceles")
+        elif angle[0] == angle[1] and angle[1] == angle[2]:
+            print("Equilateral")
+        else:
+            print("Scalene")
+    
+    
+    
