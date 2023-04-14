5329925e5ecb6957196cc034da4524d8913147fc
GunukLyoo <gunuk418@gmail.com>
2023-04-14 23:38:22 +0900

    ing

--- to python/Q1260.py
+++ to python/Q1260.py
@@ -7,11 +7,17 @@ if __name__ == '__main__':
     for i in range(m):
         li.append(list(map(int, sys.stdin.readline().split())))
 
-    dfs = sorted(li, key = lambda x: (x[0], v))
+    dfs = sorted(li, key = lambda x: x[0])
 
-    bfs = sorted(li, key = lambda x: (x[1], v))
+    bfs = sorted(li, key = lambda x: x[1])
 
     print(dfs)
     print(bfs)
 
+    ct = m
+    p = 0
+
+    for i in range(n):
+        if dfs[i][0] == v:
+            p = i
     
