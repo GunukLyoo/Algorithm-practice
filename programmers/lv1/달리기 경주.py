a21a2539d8efe3b9434432eaaeacc4879c46b275
GunukLyoo <gunuk418@gmail.com>
2023-04-29 23:55:33 +0900

    need to make faster

--- programmers/lv1/달리기 경주.py
+++ programmers/lv1/달리기 경주.py
@@ -3,19 +3,15 @@ def solution(players, callings):
     ad = dict()
     for i in range(len(players)):
         ad[i+1] = players[i]   
-    count = 1
-    for i in range(1, len(callings)):
-        if players[i-1] == players[i]:
-            count += 1
-        else:
-            j = players.index(players[i-1])
-            print(players[i-1])
-            x = players[j]
-            for i in range(1, count+1):
-                players[j-(i-1)] = players[j-i]
-            players[j-count] = x
-            count = 1
+
+    for i in range(len(callings)):
+        for j in range(len(players)):
+            if ad[j+1] == callings[i]:
+                ad[j+1] = ad[j]
+                ad[j] = callings[i]
+                break
         
-    answer = players
+    for i in ad.values():
+        answer.append(i)
     
     return answer
