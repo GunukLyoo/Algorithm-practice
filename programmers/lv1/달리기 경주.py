b1ad7bc243ee80ac5679e808c7caed7ab22ae413
GunukLyoo <gunuk418@gmail.com>
2023-05-06 23:41:23 +0900

    idea checking

--- programmers/lv1/달리기 경주.py
+++ programmers/lv1/달리기 경주.py
@@ -1,9 +1,15 @@
 def solution(players, callings):
     answer = []
-    count = 0
+    count = 1
 
-    for i in range(len(callings)):
-        for j in range(1, len(players)):
+    for i in range(1, len(callings)):
+        if callings[i-1] == callings[i]:
+            count += 1
+        else:
+            count = 1
+
+
+    for j in range(1, len(players)):
             if players[j] == callings[i]:
                 players[j] = players[j-1]
                 players[j-1] = callings[i]
