2d1518a0d7550ca1207f64706a6708cf1893e046
GunukLyoo <gunuk418@gmail.com>
2023-05-12 23:32:57 +0900

    need to fix

--- programmers/lv1/덧칠하기.py
+++ programmers/lv1/덧칠하기.py
@@ -1,10 +1,10 @@
 def solution(n, m, section):
     answer = 0
-    count = 0
 
     for i in range(0, len(section), m):
         for j in range(i+1, len(section)):
             if m >= section[j] - section[i]:
-                count += 1
-            
+                answer += 1
+                break
+
     return answer
