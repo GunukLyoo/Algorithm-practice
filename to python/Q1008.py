078406a10060b856af80f9ab6ccd3c4b1b1422f7
GunukLyoo <gunuk418@gmail.com>
2023-05-21 23:49:29 +0900

    change

--- to python/Q1008.py
+++ to python/Q1008.py
@@ -1,5 +1,6 @@
+import sys
 
 if __name__ == "__main__":
-    a, b = map(int, input().split())
+    a, b = map(int, sys.stdin.readline().split())
     sum = a / b
     print(sum)
--- to python/Q10998.py
+++ to python/Q10998.py
@@ -1,6 +1,6 @@
+import sys
 import math
 
 if __name__ == "__main__":
     a = list(map(int, sys.stdin.readline().split()))
-    m = math.prod(a)
-    print(m)
+    print(math.prod(a))
