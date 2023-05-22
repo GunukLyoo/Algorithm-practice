b79d359f0aaad7f525c1ced5605aa39100c489e2
GunukLyoo <gunuk418@gmail.com>
2023-05-22 23:02:51 +0900

    use sys

--- to python/Q10869.py
+++ to python/Q10869.py
@@ -1,6 +1,7 @@
+import sys
 
 if __name__ == "__main__":
-    a, b = map(int, input().split())
+    a, b = map(int, sys.stdin.readline().split())
     print(a + b)
     print(a - b)
     print(a * b)
