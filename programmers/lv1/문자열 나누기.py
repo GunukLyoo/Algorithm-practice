89a084e8420235cbdda215b69e0336cfb45bf288
GunukLyoo <gunuk418@gmail.com>
2023-06-05 23:17:32 +0900

    something wrong

--- programmers/lv1/문자열 나누기.py
+++ programmers/lv1/문자열 나누기.py
@@ -1,25 +1,27 @@
 def solution(s):
     answer = 0
-    cx = 1
+    cx = 0
     c = 0
     
     while len(s) > 0:
         if len(s) == 1:
             answer += 1
         else:
-            cx = 1
-            for i in range(1, len(s)):
-                if cx == c:
-                    answer += 1
-                    s = s[i+1:]
-                    c = 0
-                    break
-                    
-                if s[0] == s[i]:
+            cx = 0
+            c = 0
+            for i in range(len(s)):
+                if i == 0:
+                    cx = 1
+                elif s[0] == s[i]:
                     cx += 1
                 else:
                     c += 1
                 
-            print(s)
-                   
+                if cx == c:
+                    answer += 1
+                    s = s[i+1:]
+                    print(s)
+                    c = 0
+                    break
+        
     return answer
