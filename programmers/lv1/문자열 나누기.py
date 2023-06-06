f6724ee8286a5b4d347b4fb442973cd2448783a5
GunukLyoo <gunuk418@gmail.com>
2023-06-06 22:51:06 +0900

    need to fix more

--- programmers/lv1/문자열 나누기.py
+++ programmers/lv1/문자열 나누기.py
@@ -3,25 +3,28 @@ def solution(s):
     cx = 0
     c = 0
     
-    while len(s) > 0:
+    while len(s) >= 1:
+        for i in range(len(s)):
+            if i == 0:
+                cx = 1
+            elif s[0] == s[i]:
+                cx += 1
+            else:
+                c += 1
+                
+            if cx == c:
+                answer += 1
+                s = s[i+1:]
+                cx = 0
+                c = 0
+                break
+                
+            if i == len(s)-1:
+                answer += 1
+                break
+                    
         if len(s) == 1:
             answer += 1
-        else:
-            cx = 0
-            c = 0
-            for i in range(len(s)):
-                if i == 0:
-                    cx = 1
-                elif s[0] == s[i]:
-                    cx += 1
-                else:
-                    c += 1
-                
-                if cx == c:
-                    answer += 1
-                    s = s[i+1:]
-                    print(s)
-                    c = 0
-                    break
+            break
         
     return answer
