c9b0250f625224814623e7dc56986ec3aa16a451
GunukLyoo <gunuk418@gmail.com>
2023-06-09 22:28:35 +0900

    ing

---
+++ programmers/lv1/대충 만든 자판.py
@@ -0,0 +1,13 @@
+def solution(keymap, targets):
+    answer = []
+    count = 0
+    x = [0 for i in range(len(keymap))]
+    print(x)
+    
+    for i in range(len(targets)):
+        try:
+            
+        except ValueError:
+    
+    
+    return answer
