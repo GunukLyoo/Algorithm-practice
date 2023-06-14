ea26c9cfdf0b1d10bd366f68dba0c6cf74314d4f
GunukLyoo <gunuk418@gmail.com>
2023-06-14 23:30:39 +0900

    ing

---
+++ programmers/lv1/가장 가까운 같은 글자.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = []
+    dc = {}
+    
+    for i in range(len(s)):
+        if s[i] in dc:
+            answer.append(i-dc[s[i]])
+            dc[s[i]] = i
+        else:
+            answer.append(-1)
+            dc[s[i]] = -1
+    return answer
