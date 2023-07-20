31e10dd3d3f0bfe2efbf16681c8476d7405aeb7b
GunukLyoo <gunuk418@gmail.com>
2023-07-20 22:42:24 +0900

    need to make faster

--- programmers/lv1/달리기 경주.py
+++ programmers/lv1/달리기 경주.py
@@ -1,10 +1,18 @@
 def solution(players, callings):
     answer = []
-    count = 1
-
-    for i in range(len(callings)):
-        x = players.index(callings[i])
-        players[x] = players[x-1]
-        players[x-1] = callings[i]
-            
-    return players
+    bf = [[players[i],i+1] for i in range(len(players))]
+    ix = 0
+    for i in callings:
+        for j in range(len(bf)):
+            if bf[j][0] == i:
+                ix = bf[j][1]
+                for k in range(len(bf)):
+                    if bf[k][1] == ix-1:
+                        bf[k][1] += 1
+                bf[j][1] -= 1
+        
+    bf.sort(key = lambda x:x[1])
+    
+    answer = [bf[i][0] for i in range(len(bf))]
+    
+    return answer
