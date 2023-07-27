ce575c8589f58abe86f1e4c7d6e68bad238f564d
GunukLyoo <gunuk418@gmail.com>
2023-07-27 22:29:42 +0900

    ing

--- programmers/lv1/개인정보 수집 유효기간.py
+++ programmers/lv1/개인정보 수집 유효기간.py
@@ -2,4 +2,8 @@ def solution(today, terms, privacies):
     answer = []
     li = [i.split(" ") for i in terms]
     print(li)
+    
+    p = [i.replace('.', ' ') for i in privacies]
+    lip = [i.split(" ") for i in p]
+    print(lip)
     return answer
