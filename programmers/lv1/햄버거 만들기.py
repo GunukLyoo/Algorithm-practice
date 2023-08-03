ad6158cf57d06ae0437cdf981a47ce7f590ecc38
GunukLyoo <gunuk418@gmail.com>
2023-08-03 22:38:04 +0900

    need to make faster

--- programmers/lv1/햄버거 만들기.py
+++ programmers/lv1/햄버거 만들기.py
@@ -2,14 +2,11 @@ def solution(ingredient):
     answer = 0
     rec = "1231"
     st = ""
-    check = True
     for i in ingredient:
         st += str(i)
-    while check:
-        if rec in st:
-            answer += st.count(rec)
-            st = st.replace(rec, "")
-        else:
-            check = False
+        if len(st) > 3:
+            if st[len(st)-4:len(st)] == rec:
+                answer += 1
+                st = st.replace(rec, "")
         
     return answer
