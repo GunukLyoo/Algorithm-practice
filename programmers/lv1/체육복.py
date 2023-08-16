1b8796a739cb1b39ffe374c1d86deca9b2dc75ca
GunukLyoo <gunuk418@gmail.com>
2023-08-16 23:22:57 +0900

    need to fix more

--- programmers/lv1/체육복.py
+++ programmers/lv1/체육복.py
@@ -1,5 +1,17 @@
 def solution(n, lost, reserve):
-    answer = n - len(lost)
     for i in lost:
-        
+        if i in reserve:
+            reserve.remove(i)
+            lost.remove(i)
+
+    answer = n - len(lost)
+            
+    for i in lost:
+        if i-1 in reserve:
+            answer += 1
+            reserve.remove(i-1)
+        elif i+1 in reserve:
+            answer += 1
+            reserve.remove(i+1)
+
     return answer
