86fea84c4ed8ac8f2e3d1d422576f293ea5d0d98
GunukLyoo <gunuk418@gmail.com>
2023-08-17 23:42:25 +0900

    success

--- programmers/lv1/체육복.py
+++ programmers/lv1/체육복.py
@@ -1,12 +1,16 @@
 def solution(n, lost, reserve):
+    sl = []
     for i in lost:
+        if i not in reserve:
+            sl.append(i)
         if i in reserve:
             reserve.remove(i)
-            lost.remove(i)
 
-    answer = n - len(lost)
+    answer = n - len(sl)
+    reserve.sort()
+    sl.sort()
             
-    for i in lost:
+    for i in sl:
         if i-1 in reserve:
             answer += 1
             reserve.remove(i-1)
