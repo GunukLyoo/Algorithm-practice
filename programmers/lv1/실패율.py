772038156dcd9c48fa3f016646ec44a05263e00b
GunukLyoo <gunuk418@gmail.com>
2023-08-18 22:36:33 +0900

    need to fix

---
+++ programmers/lv1/실패율.py
@@ -0,0 +1,12 @@
+def solution(N, stages):
+    answer = []
+    dct = {}
+    p = len(stages)
+    for i in range(N):
+        x = stages.count(i+1)
+        dct[i+1] = x/p
+        p -= x
+    sd = sorted(dct.items(), key = lambda item: item[1], reverse = True)
+    for i in sd:
+        answer.append(i[0])
+    return answer
