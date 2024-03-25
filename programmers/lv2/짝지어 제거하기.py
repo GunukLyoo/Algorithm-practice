68ac010fcab750476cfa89052c01fbc942d124f6
GunukLyoo <32120377+GunukLyoo@users.noreply.github.com>
2024-03-25 23:48:19 +0900

    ing

--- programmers/lv2/짝지어 제거하기.py
+++ programmers/lv2/짝지어 제거하기.py
@@ -5,16 +5,26 @@ def solution(s):
     s1 = ""
 
     while c < 0:
+        if len(st) == 0:
+            c = 1
+        
         for i in range(0, len(st)-1):
             print(i)
             if st[i] == st[i+1]:
-                continue               
+                s1 += st[i+2:]
+                break
             else:
-                s1 += st[i]
+                if i == len(st)-1:
+                    s1 += st[i+1]
+                    c = 0
+                    break
+                else:
+                    s1 += st[i]
         print(st)
         print(s1)
         st = s1
         s1 = ""
-        c = 0
+        
+    answer = c
 
     return answer
