9f5108328ec51008f8a70d008a67fedf5027adaa
GunukLyoo <32120377+GunukLyoo@users.noreply.github.com>
2024-03-26 23:32:51 +0900

    ing

--- programmers/lv2/짝지어 제거하기.py
+++ programmers/lv2/짝지어 제거하기.py
@@ -9,19 +9,17 @@ def solution(s):
             c = 1
         
         for i in range(0, len(st)-1):
-            print(i)
             if st[i] == st[i+1]:
                 s1 += st[i+2:]
                 break
             else:
-                if i == len(st)-1:
+                if i == len(st)-2:
+                    s1 += st[i]
                     s1 += st[i+1]
                     c = 0
                     break
                 else:
                     s1 += st[i]
-        print(st)
-        print(s1)
         st = s1
         s1 = ""
         
