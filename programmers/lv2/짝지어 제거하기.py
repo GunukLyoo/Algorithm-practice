021982bcc1d0c62df54e69601a0608b80db46186
GunukLyoo <32120377+GunukLyoo@users.noreply.github.com>
2024-03-30 23:35:06 +0900

    ing

--- programmers/lv2/짝지어 제거하기.py
+++ programmers/lv2/짝지어 제거하기.py
@@ -3,24 +3,24 @@ def solution(s):
     c = -1
     st = s[:]
     s1 = ""
+    se = set(st)
 
     while c < 0:
         if len(st) == 0:
             c = 1
         
-        for i in range(0, len(st)-1):
-            if st.count(st[i]*2):
-                s1 = st.replace(st[i]*2, '')
+        for i in range(0, len(se)):
+            if st.count(se[i]*2):
+                s1 = st.replace(se[i]*2, '')
                 break
             else:
-                if i == len(st)-2:
+                if i == len(se)-1:
                     c = 0
                     break
-                else:
-                    s1 += st[i]
         print(s1)
         st = s1
         s1 = ""
+        se = set(st)
         
     answer = c
 
