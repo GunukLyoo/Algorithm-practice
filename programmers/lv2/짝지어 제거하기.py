c3523623f3196910794e9a9fb97e50caaa28d556
GunukLyoo <32120377+GunukLyoo@users.noreply.github.com>
2024-04-01 23:47:46 +0900

    wrong

--- programmers/lv2/짝지어 제거하기.py
+++ programmers/lv2/짝지어 제거하기.py
@@ -12,7 +12,6 @@ def solution(s):
         for i in range(0, len(se)):
             if st.count(se[i]*2):
                 s1 = st.replace(se[i]*2, '')
-                se.remove(se[i])
                 break
             else:
                 if i == len(se)-1:
@@ -22,5 +21,3 @@ def solution(s):
         s1 = ""
         
     answer = c
-
-    return answer
