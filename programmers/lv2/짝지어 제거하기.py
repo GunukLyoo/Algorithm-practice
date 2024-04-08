6edfc830fd8a47f7b7e161a85fbe324c64615045
GunukLyoo <32120377+GunukLyoo@users.noreply.github.com>
2024-04-08 22:35:29 +0900

    ing

--- programmers/lv2/짝지어 제거하기.py
+++ programmers/lv2/짝지어 제거하기.py
@@ -6,12 +6,16 @@ def solution(s):
     se = list(set(st))
 
     while c < 0:
+        if len(s)%2 == 1:
+            c = 0
+        
         if len(st) == 0:
             c = 1
         
         for i in range(0, len(se)):
             if st.count(se[i]*2):
                 s1 = st.replace(se[i]*2, '')
+                se.remove(se[i])
                 break
             else:
                 if i == len(se)-1:
@@ -21,3 +25,5 @@ def solution(s):
         s1 = ""
         
     answer = c
+
+    return answer
